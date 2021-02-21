5c5f1582cb89e6d604b34ba47623db89db91b5a0
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-02-21 13:36:28 +0000

    setting up Nuget and API

---
+++ Domain/Activity.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+
+        public string Description { get; set; }
+
+        public string Catergory { get; set; }
+
+        public string Venus { get; set; }
+
+    }
+}
